a5dca713810f8090c5ea28fa7415f250941afaaa
TukangLas21 <19623160@std.stei.itb.ac.id>
2025-03-29 14:26:57 +0700

    feat: add min_size attr

--- src/Quadtree.java
+++ src/Quadtree.java
@@ -1,11 +1,13 @@
 
 public class Quadtree {
     public Node root;
-    public int threshold; 
+    public double threshold; 
+    public int min_size;
 
-    public Quadtree(int threshold) {
+    public Quadtree(int threshold, int min_size) {
         this.root = null; // Inisialisasi root sebagai null
         this.threshold = threshold; // Set threshold untuk pembagian
+        this.min_size = min_size; // Set ukuran minimum untuk pembagian
     }
 
     // Placeholder CreateTree dan CreateNodes
