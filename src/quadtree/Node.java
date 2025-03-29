b3ad86aee4cd949dd40661a1f58f5e64c9587912
karolyangqian <karolyangqian14@gmail.com>
2025-03-29 22:55:30 +0700

    feat: quatdree construction

--- src/quadtree/Node.java
+++ src/quadtree/Node.java
@@ -1,16 +1,16 @@
 package quadtree;
 public class Node {
-    int x, y; // Koordinat blok (piksel kiri atas)
-    int width, height; // Ukuran blok
-    int[][][] arrRGB;
-    Node[] children; // Array untuk menyimpan anak-anak node
+    int x, y; 
+    int width, height; 
+    int argb;
+    Node[] children;
 
-    public Node(int x, int y, int width, int height, int[][][] arrRGB) {
+    public Node(int x, int y, int width, int height, int argb) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.arrRGB = arrRGB;   
+        this.argb = argb;
         this.children = null; 
     }
 
--- src/quadtree/Quadtree.java
+++ src/quadtree/Quadtree.java
@@ -1,37 +1,121 @@
 package quadtree;
+import java.awt.image.BufferedImage;
 public class Quadtree {
     public Node root;
     public double threshold; 
     public int min_size;
+    public BufferedImage imageData;
 
     public Quadtree(int threshold, int min_size) {
-        this.root = null; // Inisialisasi root sebagai null
-        this.threshold = threshold; // Set threshold untuk pembagian
-        this.min_size = min_size; // Set ukuran minimum untuk pembagian
+        this.root = null; 
+        this.threshold = threshold; 
+        this.min_size = min_size; 
+        this.imageData = null; 
     }
 
-    // Placeholder CreateTree dan CreateNodes
-    public void CreateTree(int x, int y, int[][][] imageData) {
-
+    public void CreateQuadtree(BufferedImage imageData, String errorMethod) {
+        this.imageData = imageData; 
+        int width = imageData.getWidth(); 
+        int height = imageData.getHeight(); 
+        this.root = new Node(0, 0, width, height, imageData.getRGB(0, 0)); 
+        BuildQuadTree(root, imageData, errorMethod); 
     }
 
-    public Node CreateNodes(int x, int y, int width, int height, int[][][] imageData) {
-        return new Node(x, y, width, height, imageData); // Placeholder
-    }
+    // public Node CreateNodes(int x, int y, int width, int height, int[][][] imageData) {
+    //     return new Node(x, y, width, height, imageData); // Placeholder
+    // }
 
-    public void CreateChildren(Node node, int[][][] imageData) {
-        int halfWidth = node.width / 2;
-        int halfHeight = node.height / 2;
-        
-        if (halfWidth * halfHeight < min_size) {
-            return;
+    private void BuildQuadTree(Node node, BufferedImage imageData, String errorMethod) {
+
+        if (node == null) {
+            return; 
+        }
+        if (node.width <= 1 && node.height <= 1) {
+            return; 
         }
 
-        node.children = new Node[4];
-        node.children[0] = CreateNodes(node.x, node.y, halfWidth, halfHeight, imageData); // Kiri Atas
-        node.children[1] = CreateNodes(node.x + halfWidth, node.y, halfWidth, halfHeight, imageData); // Kanan Atas
-        node.children[2] = CreateNodes(node.x, node.y + halfHeight, halfWidth, halfHeight, imageData); // Kiri Bawah
-        node.children[3] = CreateNodes(node.x + halfWidth, node.y + halfHeight, halfWidth, halfHeight, imageData); // Kanan Bawah
+        int leftHalfWidth = (node.width + 1) / 2;
+        int topHalfHeight = (node.height + 1) / 2;
+        int rightHalfWidth = node.width - leftHalfWidth;
+        int bottomHalfHeight = node.height - topHalfHeight;
 
+        double topLeftError = 0;
+        double topRightError = 0;
+        double bottomLeftError = 0;
+        double bottomRightError = 0;
+
+        switch (errorMethod) {
+            case "variance" -> {
+                // TODO: Implementasi error variance
+            }
+            case "mad" -> {
+                // TODO: Implementasi error mean absolute deviation
+            }
+            case "mpd" -> {
+                // TODO: Implementasi error mean pixel difference
+            }
+            case "entropy" -> {
+                // TODO: Implementasi error entropy
+            }
+            case "ssim" -> {
+                // TODO: Implementasi error structural similarity index
+            }
+            default -> {
+            }
+        }
+        
+        // if (error < threshold || halfWidth * halfHeight < min_size) {
+        //     return;
+        // }
+
+        // TODO: gunakan mean untuk mengisi warna node
+        node.children = new Node[4];
+        
+        if (topHalfHeight * leftHalfWidth >= min_size && topLeftError > threshold) {
+            node.children[0] = new Node(node.x, node.y, leftHalfWidth, topHalfHeight, imageData.getRGB(node.x, node.y)); // Kiri Atas
+        } else {
+            node.children[0] = null; 
+        }
+        if (topHalfHeight * rightHalfWidth >= min_size && topRightError > threshold) {
+            node.children[1] = new Node(node.x + leftHalfWidth, node.y, rightHalfWidth, topHalfHeight, imageData.getRGB(node.x + leftHalfWidth, node.y)); // Kanan Atas
+        } else {
+            node.children[1] = null; 
+        }
+        if (bottomHalfHeight * leftHalfWidth >= min_size && bottomLeftError > threshold) {
+            node.children[2] = new Node(node.x, node.y + topHalfHeight, leftHalfWidth, bottomHalfHeight, imageData.getRGB(node.x, node.y + topHalfHeight)); // Kiri Bawah
+        } else {
+            node.children[2] = null; 
+        }
+        if (bottomHalfHeight * rightHalfWidth >= min_size && bottomRightError > threshold) {
+            node.children[3] = new Node(node.x + leftHalfWidth, node.y + topHalfHeight, rightHalfWidth, bottomHalfHeight, imageData.getRGB(node.x + leftHalfWidth, node.y + topHalfHeight)); // Kanan Bawah
+        } else {
+            node.children[3] = null; 
+        }
+
+        for (Node child : node.children) {
+            BuildQuadTree(child, imageData, errorMethod);
+        }
+    }
+
+    public BufferedImage ImageFromQuadtree() {
+        BufferedImage newImageData = new BufferedImage(root.width, root.height, BufferedImage.TYPE_INT_ARGB); 
+        FillImageData(root, newImageData); 
+        return newImageData; 
+    }
+
+    public void FillImageData(Node node, BufferedImage imageData) {
+        if (node == null) {
+            return; 
+        }
+        for (int i = node.x; i < node.x + node.width; i++) {
+            for (int j = node.y; j < node.y + node.height; j++) {
+                imageData.setRGB(i, j, node.argb);
+            }
+        }
+        if (node.children != null) {
+            for (Node child : node.children) {
+                FillImageData(child, imageData);
+            }
+        }
     }
 }
