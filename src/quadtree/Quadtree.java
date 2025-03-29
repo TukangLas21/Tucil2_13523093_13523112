5300a79a8a0690b0790b3613a28430c8466d4c79
karolyangqian <karolyangqian14@gmail.com>
2025-03-29 15:58:09 +0700

    feat: create children in progress

--- src/quadtree/Quadtree.java
+++ src/quadtree/Quadtree.java
@@ -19,4 +19,19 @@ public class Quadtree {
         return new Node(x, y, width, height, imageData); // Placeholder
     }
 
+    public void CreateChildren(Node node, int[][][] imageData) {
+        int halfWidth = node.width / 2;
+        int halfHeight = node.height / 2;
+        
+        if (halfWidth * halfHeight < min_size) {
+            return;
+        }
+
+        node.children = new Node[4];
+        node.children[0] = CreateNodes(node.x, node.y, halfWidth, halfHeight, imageData); // Kiri Atas
+        node.children[1] = CreateNodes(node.x + halfWidth, node.y, halfWidth, halfHeight, imageData); // Kanan Atas
+        node.children[2] = CreateNodes(node.x, node.y + halfHeight, halfWidth, halfHeight, imageData); // Kiri Bawah
+        node.children[3] = CreateNodes(node.x + halfWidth, node.y + halfHeight, halfWidth, halfHeight, imageData); // Kanan Bawah
+
+    }
 }
